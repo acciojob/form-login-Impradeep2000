aa4a94d0c0e060e7651a27a40971c38bef5904c1
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-21 14:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const firstName = document.getElementByName("fname");
+	const lastName = document.getElementByName("lname");
+	alert(firstName +" "+lastName);
 }
